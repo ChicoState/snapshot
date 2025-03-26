f3e94730dfa47eef0c81745530412cb46f32b332
Tyler <twenke@csuchico.edu>
2025-03-26 12:26:25 -0700

    fix linux compatibility

--- ocr.py
+++ ocr.py
@@ -1,6 +1,11 @@
 import pytesseract
 from PIL import Image
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+import os
+
+if os.name == "nt":  # Windows
+    pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+elif os.name == "posix": # Linux
+    pytesseract.pytesseract.tesseract_cmd = "/usr/bin/tesseract"
 
 def extract_text(image_path):
     """Extract text from an image using Tesseract OCR."""
