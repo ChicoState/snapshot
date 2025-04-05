170a789c87d9160cb803d284e604e479655c07ee
MPawek <mpawek@gmail.com>
2025-04-04 17:00:14 -0700

    Added ability to retain previous settings choices between instances

--- Settings.py
+++ Settings.py
@@ -1,9 +1,10 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QGroupBox, QPushButton, QLabel, QRadioButton, QCheckBox
 from PyQt5.QtGui import QPixmap, QImage
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QSettings
 from PIL import ImageGrab
 import io
+import json
 
 class UI(QWidget):
     def __init__(self):
@@ -19,20 +20,13 @@ class UI(QWidget):
         self.capture_button.clicked.connect(self.take_screenshot)
         self.layout.addWidget(self.capture_button)
 
-
-
-
-        # Start of menu code
-        # Button to access menu
-        self.options_button = QPushButton("Options")
+        # Start of Settings code
+        # Button to access settings
+        self.options_button = QPushButton("Settings")
 
         self.options_button.clicked.connect(self.open_settings)
         self.layout.addWidget(self.options_button)
-        # End of menu code
-
-
-
-
+        # End of Settings code
 
         # Image preview label
         self.image_label = QLabel("Screenshot will appear here.")
@@ -61,34 +55,26 @@ class UI(QWidget):
         ))
     # End Screenshot Code
 
-
-
-
-    # Menu Code
-    # This code was from an example, have to confirm it works as expected
+    # When Settings button is pressed:
     def open_settings(self):
         self.open_settings = Settings()
         self.open_settings.show()
-    # Open settings window here
         
+# Settings Window:        
 class Settings(QWidget):
     def __init__(self):
         super().__init__() #initalizing the parent class constructor 
 
-        # Setup window
-
-        # Window name, size, and layout
+        # Window name, size, and layout. Also indicates this is a settings window, which means changes should persist
         self.setWindowTitle("Settings") 
+        self.settings = QSettings("Software Engineering Class", "Snapshot")
         self.setGeometry(100, 100, 800, 600)
         self.main_layout = QVBoxLayout()
 
         # Notification settings group box
-        self.notification_group_box = QGroupBox("Notification Settings:")
-        self.notification_button1 = QRadioButton("Show notifications")
-        self.notification_button2 = QRadioButton("Do not show notifications")
-
-        # Set default to show notifications
-        self.notification_button1.setChecked(True)
+        self.notification_group_box = QGroupBox("Notification Settings: ")
+        self.notification_button1 = QRadioButton("Show Notifications")
+        self.notification_button2 = QRadioButton("Don't Show Notifications")
 
         # Set up notification group box layout
         self.notification_layout = QVBoxLayout()
@@ -100,12 +86,9 @@ class Settings(QWidget):
         self.main_layout.addWidget(self.notification_group_box)
 
         # Open file settings group box
-        self.open_file_group_box = QGroupBox("File Settings:")
-        self.open_file_button1 = QRadioButton("Open file after Snapshot")
-        self.open_file_button2 = QRadioButton("Do not open file after Snapshot")
-
-        # Set default to open file after Snapshot
-        self.open_file_button1.setChecked(True)
+        self.open_file_group_box = QGroupBox("File Settings: ")
+        self.open_file_button1 = QRadioButton("Open Destination File")
+        self.open_file_button2 = QRadioButton("Don't Open Destination File")
 
         # Set up open file group box layout
         self.open_file_layout = QVBoxLayout()
@@ -116,14 +99,12 @@ class Settings(QWidget):
         # Add open file setting group box to main settings window
         self.main_layout.addWidget(self.open_file_group_box)
 
+        # Note: Should try and see if there is a setting that makes sure at least one box must be checked at all times to prevent user error
         # Text destination settings group box
         self.text_destination_group_box = QGroupBox("Text Destination Settings:")
         self.text_destination_button1 = QCheckBox("Save text to clipboard")
         self.text_destination_button2 = QCheckBox("Save text to new file")
         self.text_destination_button3 = QCheckBox("Save text to old file")
-
-        # Set default to new file
-        self.text_destination_button2.setCheckState(True)
 
         # Set up text destination group box layout
         self.text_destination_layout = QVBoxLayout()
@@ -145,23 +126,88 @@ class Settings(QWidget):
         # Finalize layout
         self.setLayout(self.main_layout)
 
+        # Load previously saved settings
+        self.load_settings()
+
+    # Process of loading previously saved settings:
+    def load_settings(self):
+        # Settings are stored in a JSON file
+        
+        # Notification Settings
+        # Default to show notifications
+        notification_settings = self.settings.value("Notification Settings: ", "Show Notifications")
+        if notification_settings == "Show Notifications":
+            self.notification_button1.setChecked(True)
+        else:
+            self.notification_button2.setChecked(True)
+
+        # Open File Settings
+        # Default to open file after Snapshot
+        open_file_settings = self.settings.value("File Settings: ", "Open Destination File")
+        if open_file_settings == "Open Destination File":
+            self.open_file_button1.setChecked(True)
+        else:
+            self.open_file_button2.setChecked(True)
+
+
+        # Text Destination Settings, stored in JSON
+        # Default to new file
+        text_destination_json_settings = self.settings.value("Text Destination: ", json.dumps(["Save Text to New File"]))
+        try:
+            text_destination = json.loads(text_destination_json_settings)
+        except json.JSONDecodeError:
+            text_destination = []
+
+        self.text_destination_button1.setChecked("Save Text to Clipboard" in text_destination)
+        self.text_destination_button2.setChecked("Save Text to New File" in text_destination)
+        self.text_destination_button3.setChecked("Save Text to Old File" in text_destination)
+
+
     # When you click apply settings button:
     def apply_settings(self):
+        # Apply new settings
+        # Notification Setting:
+        if self.notification_button1.isChecked():
+            notification_setting = "Show Notifications" 
+        else:
+            notification_setting = "Don't Show Notifications"
+
+        self.settings.setValue("Notification Settings: ", notification_setting)
+
+        # Open File Setting:
+        if self.open_file_button1.isChecked():
+            file_setting = "Open Destination File"     
+        else:
+            file_setting = "Don't Open Destination File"
+
+        self.settings.setValue("File Settings: ", file_setting)
+
+        # Text Destination Setting:
+        text_destination = []
+
+        if self.text_destination_button1.isChecked():
+            text_destination.append("Save Text to Clipboard")
+        if self.text_destination_button2.isChecked():
+            text_destination.append("Save Text to New File")
+        if self.text_destination_button3.isChecked():
+            text_destination.append("Save Text to Old File")
+
+        self.settings.setValue("Text Destination: ", json.dumps(text_destination))
+
         # Placeholder code to show what you've applied:
-        # In future, options should be saved to reflect the choices made
-        print("Settings applied:")
+        print("Settings applied: ")
 
         # Notification settings
         if self.notification_button1.isChecked():
             print("Notification: Show")
         else:
-            print("Notification: Do not show")
+            print("Notification: Don't show")
 
         # File settings
         if self.open_file_button1.isChecked():
             print("File Settings: Open file after Snapshot")
         else:
-            print("File Settings: Do not open file after Snapshot")
+            print("File Settings: Don't open file after Snapshot")
 
         # Text destination settings
         if self.text_destination_button1.isChecked():
