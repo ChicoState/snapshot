6c9f2d661a0a4a73941c874ef927fd6b8fbb4baa
bdaqiq01 <basira.daqiq0@gmail.com>
2025-04-27 15:30:56 -0700

    added documents default path, and enabled user to enter custom path

--- UI.py
+++ UI.py
@@ -6,7 +6,7 @@ from PyQt5.QtCore import Qt, QSettings
 from io import BytesIO 
 import json
 from screenshot import take_screenshot, process_ocr
-
+import os
 
 class UI(QWidget):
     def __init__(self):
@@ -16,23 +16,28 @@ class UI(QWidget):
         self.setGeometry(100, 100, 800, 600)
 
         self.layout = QVBoxLayout() #shows the window layout
-
-        # button to take screenshot
+        self.settings = QSettings("Software Engineering Class", "Snapshot")
+        # button to take screenshot calles the self.screenshot_text function which takes the screenshot 
         self.capture_button = QPushButton("Take Screenshot")
+        
+        self.default_filename = "extracted_text.txt"
+        saved_path = self.settings.value("text_destination", "")
+        if saved_path:
+            self.file_path = saved_path
+        else:
+            self.file_path = os.path.join(os.path.expanduser("~"), "Documents", self.default_filename)
 
+        
         self.capture_button.clicked.connect(self.screenshot_text) #connects the screenshot tool to the botton on the screen 
         self.layout.addWidget(self.capture_button)
 
         # Start of Settings code
-        # Button to access settings
+        # Button to access settings connects to the self.open_settings when it is pressed
         self.options_button = QPushButton("Settings")
-
         self.options_button.clicked.connect(self.open_settings)
         self.layout.addWidget(self.options_button)
         # End of Settings code
-
         # Image preview label
-
         self.image_label = QLabel("Screenshot will appear here.")
         self.image_label.setAlignment(Qt.AlignCenter)
         self.layout.addWidget(self.image_label)
@@ -41,7 +46,7 @@ class UI(QWidget):
 
     # When Settings button is pressed:
     def open_settings(self):
-        self.open_settings = Settings()
+        self.open_settings = Settings(self)
         self.open_settings.show()
 
     def screenshot_text(self):
@@ -50,6 +55,23 @@ class UI(QWidget):
 
         if cropped_image:
             # Convert PIL Image to QPixmap
+            #
+            if not self.file_path:
+                
+
+                file_name, _ = QFileDialog.getSaveFileName(
+                    self, 
+                    "Select File Destination", self.default_filename, 
+                    "Text Files (*.txt)"
+                )
+                if file_name:
+                    self.file_path = file_name
+                    self.settings.setValue("text_destination", self.file_path)
+                else:
+                    # If user cancels, don't proceed
+                    self.image_label.setText("No save location selected.")
+                    return
+            #
             buffer = BytesIO()
             cropped_image.save(buffer, format="PNG")
             buffer.seek(0)
@@ -60,21 +82,22 @@ class UI(QWidget):
 
             #calling the process ocr to take the screen and save it
             # you can also pass a custom path to the process_ocr to save it in a custom location  
-            process_ocr(cropped_image= cropped_image) #converts to texts and saves
+            process_ocr(cropped_image= cropped_image, save_path= self.file_path) #converts to texts and saves to the specified location
+            os.startfile(self.file_path)
         else:
             self.image_label.setText("No image captured.")
         
 # Settings Window:        
 class Settings(QWidget):
-    def __init__(self):
+    def __init__(self, UI_window):
         super().__init__() #initalizing the parent class constructor 
 
         # Window name, size, and layout. Also indicates this is a settings window, which means changes should persist
-        self.setWindowTitle("Settings") 
+        self.setWindowTitle("Settings") #window title
         self.settings = QSettings("Software Engineering Class", "Snapshot")
         self.setGeometry(100, 100, 800, 600)
-        self.main_layout = QVBoxLayout()
-
+        self.main_layout = QVBoxLayout() #this is just the layout of the windown
+        self.UI_window = UI_window
         # Notification settings group box
         self.notification_group_box = QGroupBox("Notification Settings: ")
         self.notification_button1 = QRadioButton("Show Notifications")
@@ -105,11 +128,12 @@ class Settings(QWidget):
 
         # Note: Should try and see if there is a setting that makes sure at least one box must be checked at all times to prevent user error
         # Text destination settings group box
-        self.text_destination_group_box = QGroupBox("Text Destination Settings:")
+        self.text_destination_group_box = QGroupBox("Text Destination Settings:") #this outlines the line around the below options 
         self.text_destination_button1 = QCheckBox("Save text to clipboard")
         self.text_destination_button2 = QCheckBox("Save text to new file")
         self.text_destination_button3 = QCheckBox("Save text to old file")
-
+        
+        ##this is the layout for the manaul and automatic file destination - 
         # Set up text destination group box layout
         self.text_destination_layout = QVBoxLayout()
         self.text_destination_layout.addWidget(self.text_destination_button1)
@@ -121,23 +145,23 @@ class Settings(QWidget):
         self.main_layout.addWidget(self.text_destination_group_box)
 
         # File Name Group Box
-        self.file_path_group_box = QGroupBox("File Name Settings: ")
+        self.file_path_group_box = QGroupBox("File Name Settings: ") #this is the outline around the destination file_name field 
         self.file_browse_button = QPushButton("Browse")
 
         # Format file name group box layout
-        self.file_path_layout = QHBoxLayout()
-        self.file_path_layout.addWidget(QLabel("Text Destination File Name: "))
-        self.file_path_input = QLineEdit()
-        self.file_path_input.setReadOnly(True)
-        self.file_path_layout.addWidget(self.file_path_input)
-        self.file_path_layout.addWidget(self.file_browse_button)
-        self.file_path_group_box.setLayout(self.file_path_layout)
+        self.file_path_layout = QHBoxLayout() #creates a horizental line layout widgets will be arranged side by side and the layour is stored in the filepath layput
+        self.file_path_layout.addWidget(QLabel("Text Destination File Name: ")) #adding a text widget to the layout 
+        self.file_path_input = QLineEdit() #creates a single line tet input fiels to take in the destination input 
+        self.file_path_input.setReadOnly(True) #it is set to read only for not but displays it contents from file_browse_button at the end of load settings function
+        self.file_path_layout.addWidget(self.file_path_input) #adds the input layout field into the to horizental layout
+        self.file_path_layout.addWidget(self.file_browse_button) #adds the browse button filed to the layout
+        self.file_path_group_box.setLayout(self.file_path_layout) #sets the entire compoents as a group layout
 
         # Add file name group box to main settings window
         self.main_layout.addWidget(self.file_path_group_box)
         
         # When Browse button is clicked, go to choose_file function
-        self.file_browse_button.clicked.connect(self.choose_destination_file)
+        self.file_browse_button.clicked.connect(self.choose_destination_file) #
 
         # Button to save changed settings
         self.apply_button = QPushButton("Apply Settings")
@@ -187,6 +211,7 @@ class Settings(QWidget):
 
         # Text Destination File Name (Default is currently empty)
         self.file_path = self.settings.value("text_destination", "")
+        #self.file_path = self.settings.value("text_destination", self.file_name)
         self.file_path_input.setText(self.file_path)
 
     # When you click apply settings button:
@@ -247,12 +272,20 @@ class Settings(QWidget):
         self.settings.setValue("text_destination", self.file_path_input.text())
 
     def choose_destination_file(self):
-        self.file_name, _ = QFileDialog.getSaveFileName(
-            self, "Select Output File", "", "Text File"
+        default_path = os.path.join(os.path.expanduser("~"), "Documents", self.UI_window.default_filename)
+
+        self.file_name, _ = QFileDialog.getSaveFileName(  #opens a standard save file dialog 
+            self, 
+            "Select File Destination path", 
+            default_path,
+            "Text File"  #header of dialog the default location is emput second arg " ",
+                                                                    #Text file is the fill filter return a tuple the path and the fill_filter 
         )
         if self.file_name:
-            self.file_path_input.setText(self.file_name)
-
+            self.file_path_input.setText(self.file_name) #this basically just displays the image to the text field 
+            self.settings.setValue("text_destination", self.file_name)
+            self.UI_window.file_path = self.file_name # should update the UI file path 
+        print(self.file_name)  
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
